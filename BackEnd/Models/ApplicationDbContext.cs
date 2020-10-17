8f2e49dbf52c39139956d9debe4f39711465cc8e
yasmeen <yasmeen11898@gmail.com>
2020-10-17 16:47:49 +0200

    Add project files.

---
+++ BackEnd/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+        /// create the table for speakers
+        public DbSet<Speakers> Speakers { get; set; }
+    }
+}
---
+++ BackEnd/Models/Speakers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Speakers
+    {
+        public int Id { get; set; }
+
+        [StringLength(2000)]
+        public String Name { get; set; }
+        [StringLength(4000)]
+        public String Bio { get; set; }
+        [StringLength(1000)]
+        public  virtual String WebSite { get; set; }
+
+
+    }
+}
